6e06f31d77dee7b82a44d0414b56e2249093228d
ikollipara <ian.kollipara@cune.org>
2021-04-15 12:42:42 -0500

    :sparkles: added second goal

--- pages/ePortfolio/first_year_focus.tsx
+++ pages/ePortfolio/first_year_focus.tsx
@@ -113,6 +113,81 @@ function FirstYearFocus() {
               </Container>
             </Col>
           </Row>
+          <Row>
+            <Col lg={12}>
+              <hr />
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={1}></Col>
+            <Col lg={10}>
+              <h4 className="text-font">
+                <strong>Goal:</strong> Ingrain Technology Fundementals in the 
+                Classroom
+              </h4>
+              <h4 className="text-font">
+                <strong>Why: </strong>
+                Technology is a fundemental aspect of modern life. As such, it is of
+                vital importance that students are not only competent, but actively learn
+                and improve at, technology. I firmly believe that technology literacy is the
+                most important future skill that is not met in current education. This extends
+                beyond just simple word, excel, etc. but more into troubleshooting, coding, and 
+                problem solving.
+              </h4>
+              <h4 className="text-font">
+                <strong>How: </strong>
+                I provide a unique ability in this area, being my CS degree. As such I 
+                can help more in this technical aspects. I would begin by allowing more use
+                of technology in the classroom to begin with. This could include: projectors, 
+                laptops, embedded systems, etc. Just things to spark the mind and curiosity of
+                the children.
+                <br />
+                Next I would incorporate more problem solving based issues in computer class. Things
+                like crashing errors and how to look them up. Knowing how to identify the problem is 
+                most of the battle, and if a student can do that, then they will succeed when it comes
+                to technology.
+                <br />
+                The biggest hurdle is budget, and as such much of what I want to do will be
+                relegated for future years. In this first year I want to add troubleshooting
+                to the computer classes, along with more time dedicated to logic and problem
+                solving games. I would also have a unit dedicated to Office Suite Tool usage,
+                something that will most definitely become vital as they age.
+              </h4>
+              <Container>
+                <Row>
+                  <Col lg={4} className="d-flex flex-column align-items-center">
+                    <h5 className="heading-font">Innovator's Mindset</h5>
+                    <p className="lead text-font">
+                      <ul>
+                        <li>Problem-Finder</li>
+                        <li>Risk-Taker</li>
+                      </ul>
+                    </p>
+                  </Col>
+                  <Col lg={4} className="d-flex flex-column align-items-center">
+                    <h5 className="heading-font">Today's Classroom</h5>
+                    <p className="lead text-font">
+                      <ul>
+                        <li>Opportunities for Innovation</li>
+                      </ul>
+                    </p>
+                  </Col>
+                  <Col lg={4} className="d-flex flex-column align-items-center">
+                    <h5 className="heading-font">Tech Tools</h5>
+                    <p className="lead text-font">
+                      <ul>
+                        <li>CoolMathGames</li>
+                        <li>DayOfCode</li>
+                        <li>Integrated Classroom Environment (ICE)</li>
+                        <li>Microsoft Office</li>
+                      </ul>
+                    </p>
+                  </Col>
+                </Row>
+              </Container>
+            </Col>
+          </Row>
+
         </Container>
       </article>
     </main>
